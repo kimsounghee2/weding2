0b3cfcc4e8aea39aee76ca8571a41697f7d1223e
kimsounghee2 <kimsounghee2@nate.com>
2021-05-21 21:39:15 +0900

    first

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".she_img ul").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $(".men_img ul").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $(".products ul").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
